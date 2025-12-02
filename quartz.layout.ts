e3d4f4129d4bc1038cc5da8cfe425463e7562bad
santoshmohanram <santoshmohanram@gmail.com>
2025-12-02 18:06:50 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,7 +37,6 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
